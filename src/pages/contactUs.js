256a1a86d9ffed0a90cf572de2a512341139f83c
yushaliniepillay <yushalinie97@hotmail.com>
2020-12-20 22:57:24 +0800

    update codes

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -24,80 +24,80 @@ const ContactUs = () => (
         </div>
 
         {/* locations */}
-        <div className="container">
-            <div className="row serviceList">
-                <div className="col-md-12">
-                    <section>
-                        <h2 className="section-title"><span></span>OUR OFFICES</h2>
-                    </section>
-                </div>
 
-                <div className="col-md-6 col-sm-6 col-xs-12">
-                    <div className="single-item">
-                        <div className="fig_caption">
-                            <div className="icon">
-                                <i className="fa fa-map-marker"></i>
+        <div class="container">
+            <h2 class="black-color section-title">OUR OFFICES</h2>
+            <div class="row serviceList">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="single-item">
+                        <div class="fig_caption">
+                            <div class="icon">
+                                <i class="fa fa-map-marker"></i>
                             </div>
-                            <div className="details">
-                                <h3><a href="service-signle ">MALAYSIA - HQ</a></h3>
-                                <p> Avows Technologies SDN BHD,<br />
-                                        Level 15-06, Tower A, The Vertical<br />
-                                        Business Suite,Avenue 3, Bangsar South, No 8,<br />
-                                        Jln Kerinchi, 59200	Kuala Lumpur, Malaysia.<br />
-                                        Tel: +603 27123456</p>
+                            <div class="details text-center">
+                                <h3><a href="service-signle">MALAYSIA - HQ</a></h3>
+                                <p>Avows Technologies SDN BHD,<br />
+                                    Level 15-06, Tower A, The Vertical
+                                    Business Suite,Avenue 3, Bangsar South, No 8,
+                                    Jln Kerinchi, 59200	Kuala Lumpur, Malaysia.<br />
+                                    Tel: +603 27123456
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-md-6 col-sm-6 col-xs-12">
-                    <div className="single-item">
-                        <div className="fig_caption">
-                            <div className="icon"><i class="fa fa-map-marker"></i>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="single-item">
+                        <div class="fig_caption">
+                            <div class="icon">
+                                <i class="fa fa-map-marker"></i>
                             </div>
-                            <div className="details">
+                            <div class="details text-center">
                                 <h3><a href="service-signle ">SINGAPORE</a></h3>
                                 <p>Avows Technologies Pte. Ltd.,<br />
-                                        3 Temasek Avenue, Levels 21 &amp; 34<br />
-                                        Centennial Tower, Singapore 039190.<br />
-                                        Tel : +65 6549 7000<br />
-                                        Fax: +65 6549 7001</p>
+                                    3 Temasek Avenue, Levels 21 &amp; 34
+                                    Centennial Tower, Singapore 039190.<br />
+                                    Tel : +65 6549 7000<br />
+                                    Fax: +65 6549 7001
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-md-6 col-sm-6 col-xs-12">
-                    <div className="single-item">
-                        <div className="fig_caption">
-                            <div className="icon">
-                                <i className="fa fa-map-marker"></i>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="single-item">
+                        <div class="fig_caption">
+                            <div class="icon">
+                                <i class="fa fa-map-marker"></i>
                             </div>
-                            <div className="details">
+                            <div class="details text-center">
                                 <h3><a href="service-signle ">INDONESIA</a></h3>
                                 <p>PT Avows Technologies.<br />
-                                        Office 8, Level 18A<br />
-                                        Jl. Jendral Sudirman kav. 52-53<br />
-                                        South Jakarta 12190 – Indonesia<br />
-                                        Tel : +62 2129608177</p>
+                                    Office 8, Level 18A
+                                    Jl. Jendral Sudirman kav. 52-53
+                                    South Jakarta 12190 – Indonesia<br />
+                                    Tel : +62 2129608177
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-md-6 col-sm-6 col-xs-12">
-                    <div className="single-item">
-                        <div className="fig_caption">
-                            <div className="icon">
-                                <i className="fa fa-map-marker"></i>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="single-item">
+                        <div class="fig_caption">
+                            <div class="icon">
+                                <i class="fa fa-map-marker"></i>
                             </div>
-                            <div className="details">
+                            <div class="details text-center">
                                 <h3><a href="service-signle ">USA</a></h3>
                                 <p>Avows Technologies Inc.<br />
-                                        4512 Legacy Drive #100 Plano,<br />
-                                        TX 75024<br />
-                                        Tel: +1 201 604 3505
-                                    </p>
+                                    4512 Legacy Drive #100 Plano,
+                                    TX 75024<br />
+                                    Tel: +1 201 604 3505
+                                </p>
                             </div>
                         </div>
                     </div>
