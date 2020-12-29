2da0f7e4229282eb84fe1e6df40dde1c3c26e1fb
yushaliniepillay <yushalinie97@hotmail.com>
2020-12-29 18:57:14 +0800

    fix header and footer icon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,24 +1,24 @@
-import Icon from '@ant-design/icons/lib/components/AntdIcon';
+
 import React from 'react'
 import { Button } from 'react-bootstrap';
 import "../footer/footer.css";
-
+import { FacebookFilled, LinkedinFilled, TwitterCircleFilled } from '@ant-design/icons';
 
 function footer() {
-    var icon = require('../images/avowsTitle.png')
+    var icon = require('../images/logo.png')
     var imageName = require('../images/header-slider-content/02.png')
     var imageMalay = require('../images/icon/may_icon.png');
     var imageSg = require('../images/icon/sg_icon.png');
     var imageIndo = require('../images/icon/indo_icon.png');
     var imageUSA = require('../images/icon/usa_icon.png');
     var imageIndia = require('../images/icon/taj-mahal_icon.png');
-    
+
     return (
         <footer>
             <div className="main-footer">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-3 hidden-sm hidden-xs">
+                        <div className="col-md-3 hidden-md hidden-sm hidden-xs" >
                             <div className="footer-img">
                                 {/* <img src=".\images\header-slider\04.png" alt="" className="img-responsive" /> */}
                                 <img src={imageName} alt="" className="img-responsive" />
@@ -27,7 +27,7 @@ function footer() {
                         <div className="col-md-3 col-sm-4 footer-widget">
                             <div className="footer-about">
                                 <h4 className="footer-title">ABOUT US
-                                <hr/></h4>
+                                <hr /></h4>
                                 <p className="justified">Avows Technologies founded in 2008 provides IT Consulting, Software Implementation, Development & Support services to customers in Malaysia, Indonesia, Singapore, India and USA using a Global Delivery Model backed by High Quality Talent and established best practices.</p>
                                 {/* <a href="/aboutus" className="read-more mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--accent">Read More</a> */}
                                 <Button className="m-3" variant="danger" size="md" type="submit" href="/aboutUs">READ MORE</Button>
@@ -36,7 +36,7 @@ function footer() {
                         <div className="col-md-3 col-sm-4">
                             <section className="links-widget">
                                 <h4 className="footer-title">QUICK LINKS
-                                <hr/></h4>
+                                <hr /></h4>
                                 <ul className="list-unstyled">
                                     <li><a href="/technology">SERVICES & SOLUTIONS</a></li>
                                     <li><a href="/career">CAREER</a></li>
@@ -68,7 +68,7 @@ function footer() {
                         <div className="footer-landmarks">
                             <div className="row">
                                 <div className="col-md-4">
-                                    </div>
+                                </div>
                                 <div className="col-md-1" align="center">
                                     <img src={imageMalay} alt="" /><br />
                                     <p>MALAYSIA(HQ)</p>
@@ -111,12 +111,14 @@ function footer() {
                             <div className="row">
                                 <div className="col-md-12">
                                     <div className="Social-link Social-link--circle Social-link--dark Social-link--medium">
-                                        <a href="https://www.facebook.com/avowstechnologies/?fref=ts" className="Social-link__item m-3" target="_blank">
-                                            <i className="fab fa-facebook fa-2x"></i></a>
-                                        <a href="https://www.linkedin.com/company/3651016?trk=prof-0-ovw-curr_pos" className="Social-link__item m-3" target="_blank">
-                                            <i className="fab fa-linkedin fa-2x"></i></a>
-                                        <a href="https://twitter.com/Avowstech" className="Social-link__item m-3" target="_blank">
-                                            <i className="fab fa-twitter fa-2x"></i></a>
+                                        <a href="https://www.facebook.com/avowstechnologies/?fref=ts" className="Social-link m-3" target="_blank">
+                                            <FacebookFilled className="fa-3x" />
+                                        </a>
+                                        <a href="https://www.linkedin.com/company/3651016?trk=prof-0-ovw-curr_pos" className="Social-link m-3" target="_blank">
+                                            <LinkedinFilled className="fa-3x" />
+                                        </a>
+                                        <a href="https://twitter.com/Avowstech" className="Social-link m-3" target="_blank">
+                                            <TwitterCircleFilled className="fa-3x" /></a>
                                     </div>
                                 </div>
                                 <div className="col-md-12">
