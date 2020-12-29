217a605c7d86b54b6ee50172d55063606cd561c1
yushaliniepillay <yushalinie97@hotmail.com>
2020-12-29 16:48:25 +0800

    add industries new pages

--- src/pages/industries/banking.js
+++ src/pages/industries/banking.js
@@ -2,53 +2,56 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../../components/layouts/layout"
 import SEO from "../../components/seo";
-import { Button } from 'react-bootstrap';
+import { Button, Tabs, Tab } from 'react-bootstrap';
+import ReactPlayer from 'react-player/lazy'
 
-const Banking = () => (
+const Banking = () => {
 
-    <Layout>
-        <SEO title="AVOWS" />
+    var v = require('../industries/video/PublicBank.mp4')
+    
+    return (
+        <Layout>
+            <SEO title="AVOWS" />
 
-        <div id="pageHeader" >
-            <div className="vc-parent">
-                <div className="vc-child">
-                    <section className="page-title">
-                        <h2>BANKING & FINANCIAL SOLUTION</h2>
-                        <ul className="text-center">
-                            <li className="list-unstyled text-danger">
-                                <Link className="text-white" href="/">Home /</Link> Banking</li>
-                            {/* <li className="list-unstyled active">Blog</li> */}
-                        </ul>
-                    </section>
+            <div id="pageHeader" >
+                <div className="vc-parent">
+                    <div className="vc-child">
+                        <section className="page-title">
+                            <h2>BANKING & FINANCIAL SOLUTION</h2>
+                            <ul className="text-center">
+                                <li className="list-unstyled text-danger">
+                                    <Link className="text-white" href="/">Home /</Link> Banking</li>
+                                {/* <li className="list-unstyled active">Blog</li> */}
+                            </ul>
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div id="blog">
-            <div className="container">
-                <div className="row">
-                    <div class="col-md-12 blog-page-content post-details">
-                        <div class="post-img">
+            <div id="blog" >
+                <div className="container">
+                    <div className="row">
+                        {/* <div class="post-img">
                             <img src="/img/blog-posts/blog00.jpg" alt="" class="img-responsive" />
-                        </div>
+                        </div> */}
+                        <br />
                         <div className="wpb_wrapper">
-                            <p>Banking has been a traditional consumer of IT services and a key priority 
-                                area for Avows, the recent significant developments in Digital and new 
-                                technologies has spurred a growth in the demand for impeccable service 
-                                providers across Malaysia, we cater to all businesses be it retail banking, 
-                                lending services, NBFC’s, capital markets, fixed income etc.
+                            <p class="justified">Banking has been a traditional consumer of IT services and a key priority
+                            area for Avows, the recent significant developments in Digital and new
+                            technologies has spurred a growth in the demand for impeccable service
+                            providers across Malaysia, we cater to all businesses be it retail banking,
+                            lending services, NBFC’s, capital markets, fixed income etc.
                             </p>
-                            <p>Banking Industry has been at the forefront of technology adoption, over 
-                                the past few years the way consumers interact with financial institutions 
-                                has changed a lot, thanks to the several digitization efforts taken by the 
-                                banks and other financial institutions. With the onset of Industrial Revolution 4.0 
-                                and 5G banking services, these are going to take new shapes further more.  
-                                At Avows we partner with clients in their Technology journey to enable them 
-                                to excel in their business and help them provide the best to their end consumers. 
+                            <p>Banking Industry has been at the forefront of technology adoption, over
+                            the past few years the way consumers interact with financial institutions
+                            has changed a lot, thanks to the several digitization efforts taken by the
+                            banks and other financial institutions. With the onset of Industrial Revolution 4.0
+                            and 5G banking services, these are going to take new shapes further more.
+                            At Avows we partner with clients in their Technology journey to enable them
+                            to excel in their business and help them provide the best to their end consumers.
                             </p>
-
                             <p>Our Services and Solutions:</p>
-                            <ul> 
+                            <ul>
                                 <li>Digital Banking</li>
                                 <li>Digital Transformation</li>
                                 <li>AML</li>
@@ -62,22 +65,72 @@ const Banking = () => (
                                 <li>Core Platforms</li>
                                 <li>Payment Solutions</li>
                                 <li>Card Services</li>
-                                <li>Business Process Automation</li>       
+                                <li>Business Process Automation</li>
                             </ul>
                         </div>
+
+                        <div>
+                            <br />
+                            <h2>Case Studies</h2>
+                            <Tabs defaultActiveKey="Anti" transition={false} id="noanim-tab-example">
+                                <Tab eventKey="Anti" title="Anti-Money Laundering for Merchantrade">
+                                    <br />
+                                    <h3><strong>Anti-Money Laundering for Merchantrade</strong></h3>
+                                    <section>
+                                        <h3 className="text-primary">About the Client</h3>
+                                        <p class="p1 text-justify">Client is a Money Services Business, Digital Payment Service and Mobile Virtual Network operator provider, based in Malaysia. The company’s core business is centered on international money transfers, foreign currency exchange, wholesale banknotes, digital payment processing and mobile telecommunications. It is Asia’s Leader in Digital Payments & Communications.
+                                        </p>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Challenge</h3>
+                                        <p class="p1 text-justify">
+                                            As regulatory bodies across the globe enforce stricter regulations, banks & financial institutions are facing compliance challenges. As financial activity increases along with digital means, there will be an ever-growing increase in needs to detect fraudulent money movements and patterns involving more and more digital investigations.
+                                        </p>
+                                        <p class="p1 text-justify">Client was looking to adopt an AML solution for its banking enterprise to comply with regulatory requirements through unified and consistent watch list screening at the same time also create value through effective weeding out of fraudulent participants on its network. Some of the other concerns were to have a low maintenance solution, effective with less false alarms to help optimize usage of resources.
+                                        </p>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Solution</h3>
+                                        <p class="p1 text-justify">Avows partnered with the client to provide an AML solution that matched their requirements in term of being fully equipped to weed out unnecessary noise, effective screening and comprehensive detection of suspicious transactions in real time. Our Anti-money laundering solutions comprises of proven methodologies that we use to detect more and more behavioural characteristics to classify risks and threats as authentic. Over time, our solution is capable of learning and integrating new patterns that emerge out of newer fraudulent ways the scammers or money launderers adopt hence making it increasingly difficult for them to beat the system.
+                                        </p>
+                                        <p class="p1 text-justify">
+                                            Our solution helps this remittance institutions through all AML aspects and stages. Our integrated system offers an end-to-end anti-money laundering solution with components which can also be used as stand-alone applications. This has given an opportunity to go for one-off investment by the customer to acquire multiple peripheral solutions on top of existing core applications or systems.
+                                    </p>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Benefits</h3>
+                                        <ul>
+                                            <li>Our solution incorporates modern technologies like AI, ML and robotic automation along with audio/facial recognition and document verification to allow organizations manage their compliance 100 percent and predict fraudulent behaviour patterns which reduce unaccounted damages.</li>
+                                            <li>Smart Automation with 360 degree and round the clock surveillance to combat money laundering, providing real time alerts for action.</li>
+                                            <li>Achieved real-time risk visibility and accelerate screening and monitoring with industry-proven tools -
+                                                <li>Identity Verification</li>
+                                                <li>Sanctions, PEPs and Enforcements Data</li>
+                                                <li>Account and Payment Screening</li>
+                                            </li>
+                                        </ul>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Customer Testimonial</h3>
+                                        <ReactPlayer url={v} />
+                                    </section>
+                                </Tab>
+                            </Tabs>
+                        </div>
+
+
                     </div>
                 </div>
+
+
+
+                {/* <div className="container">
+                    <Button variant="primary" href="/industries/caseStudy">Case Study</Button>{' '}
+                </div> */}
+
             </div>
 
-            <div className="container p">
-            <Button variant="primary" href="/industries/caseStudy">Case Study</Button>{' '}
-            </div>
-        </div>
 
-
-    </Layout>
-
-
-)
-
+        </Layout>
+    )
+}
 export default Banking;
---
+++ src/pages/industries/energy.js
@@ -0,0 +1,94 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../../components/layouts/layout"
+import SEO from "../../components/seo";
+import { Tabs, Tab } from 'react-bootstrap';
+
+
+const Energy = () => {
+
+    return (
+        <Layout>
+            <SEO title="AVOWS" />
+            <div id="pageHeader" >
+                <div className="vc-parent">
+                    <div className="vc-child">
+                        <section className="page-title">
+                            <h2>OILS AND GAS</h2>
+                            <ul className="text-center">
+                                <li className="list-unstyled text-danger">
+                                    <Link className="text-white" href="/">Home /</Link> Oil and Gas</li>
+                                {/* <li className="list-unstyled active">Blog</li> */}
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+            </div>
+
+            <div id="blog">
+                <div className="container">
+                    <div className="row">
+                        {/* <div class="post-img">
+                            <img src="/img/blog-posts/blog00.jpg" alt="" class="img-responsive" />
+                        </div> */}
+                        <br />
+                        <div className="wpb_wrapper">
+                            <p class="justified">
+                            </p>
+                        </div>
+
+
+                        <div>
+                            <br />
+                            <h2>Case Studies</h2>
+                            <Tabs defaultActiveKey="BI" transition={false} id="noanim-tab-example">
+                                <Tab eventKey="BI" title="BI Dashboard Development">
+                                    <br />
+                                    <h3><strong>BI Dashboard Development</strong></h3>
+                                    <section>
+                                        <h3 className="text-primary">About the Client</h3>
+                                        <p class="p1 text-justify">The client is an integrated oil and gas services company based in Malaysia. It has established its presence in over 20 countries, such as China, Australia, United States of America, and those in Western Africa and the Middle East, employing approximately 22,000 people.
+                                        </p>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Challenge</h3>
+                                        <p class="p1 text-justify">
+                                            Clients data resided in different disparate systems and lacked consistencies in bringing them together for any C level executive analysis. This resulted a very tedious exercise and took several days to achieve, hence there was an immediate need to bring data together with advanced analytics and visualization techniques to enable more insights at a faster rate and eventually enable better decision making.
+                                        </p>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Solution</h3>
+                                        <p class="p1 text-justify">Avows partnered with the client to understand the client’s organization data sets and the
+                                        different systems along with data storage access points etc. to arrive at the complete blue print for organizational data from its creating to where it was residing. We then created a solution to bring all data together for analytics and process several other information for advanced dashboards for c level executives based on discussions and business requirements.
+                                        </p>
+                                        <p class="p1 text-justify">
+                                            The solution was able to bring several benefits within a short time.
+                                    </p>
+                                    </section>
+                                    <section>
+                                        <h3 className="text-primary">Benefits</h3>
+                                        <ul>
+                                            <li>Gain a seamless environment that includes data visualization and analytics integrated with communication tools, and specialized business applications</li>
+                                            <li>Capitalize on customers’ existing Microsoft technology to accelerate progress toward the next innovation in data analytics</li>
+                                            <li>The customer has successfully migrated more than 46 metrics from 4 critical datasets from excel sheet into the SAP BW Platform for their daily, weekly and monthly reporting empowered by our design principles on Power BI platform</li>
+                                            <li>And empowered the analytics and business intelligence teams, formerly dependent on IT or support teams, to create and prepare the data themselves.</li>
+                                            <li>Direct business and executive committee to access the data. The data which took 6 days for tabulation is now available for C level reporting on the day of request.</li>
+                                        </ul>
+                                    </section>
+                                    <section>
+                                        {/* <h3 className="text-primary">Customer Testimonial</h3> */}
+                                    </section>
+                                </Tab>
+
+                            </Tabs>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </Layout>
+    )
+}
+
+export default Energy
